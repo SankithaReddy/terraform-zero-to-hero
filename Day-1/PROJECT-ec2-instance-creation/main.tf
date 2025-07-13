9cc3addf4bb1abf79fd22f2395b5fa34d6bc9fdb
SankithaReddy <48112827+SankithaReddy@users.noreply.github.com>
2025-07-13 16:13:22 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0f918f7e67a3323f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0f918f7e67a3323f0" # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id     = "subnet-0d442e25bd477099a"
+    key_name      = "snap"
 }
